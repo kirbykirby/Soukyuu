4570dd812526e7d56ab0e476dffd833fa9995f25
hendyzone dengz <hendyzone@163.com>
2013-03-03 19:02:16 +0800

    Some bugs fixed

--- soukyuu/block/InitBlock.java
+++ soukyuu/block/InitBlock.java
@@ -15,37 +15,30 @@ public class InitBlock {
 	public static Block portal;
 	
 	public InitBlock() {
-	portal = (BlockPortal) (new BlockPortal(ConfigManager.idPortal, 4));
-		portalTrigger = (BlockPortalTrigger) (new BlockPortalTrigger(
-				ConfigManager.idTrigger, 2));
-		skyStone = (BlockSkyStone) (new BlockSkyStone(ConfigManager.idStone, 1).setHardness(
-				1.7F).setResistance(11.0f)
-				.setStepSound(Block.soundStoneFootstep));
-		skyGrass = (BlockSkyGrass) (new BlockSkyGrass(ConfigManager.idGrass)
-				.setHardness(0.6F).setResistance(3.0f)
-				.setStepSound(Block.soundGrassFootstep));
-		skyDirt = (BlockSkyDirt) (new BlockSkyDirt(ConfigManager.idDirt, 2).setHardness(0.5F)
-				.setResistance(2.0f).setStepSound(Block.soundGravelFootstep));
-		skyFlower = (BlockSkyFlower) (new BlockSkyFlower(ConfigManager.idFlower, 5)
-				.setHardness(0.0F).setStepSound(Block.soundGrassFootstep));
-		present = (BlockPresent) (new BlockPresent(ConfigManager.idPresent).setHardness(0.4F));
-		skyWood = (BlockSkyWood) (new BlockSkyWood(ConfigManager.idWood).setHardness(0.7F));
+	portal = new BlockPortal(ConfigManager.idPortal, 4);
+		portalTrigger = new BlockPortalTrigger(ConfigManager.idTrigger, 2);
+		skyStone = new BlockSkyStone(ConfigManager.idStone, 1).setHardness(1.7F).setResistance(11.0f).setStepSound(Block.soundStoneFootstep);
+		skyGrass = new BlockSkyGrass(ConfigManager.idGrass).setHardness(0.6F).setResistance(3.0f).setStepSound(Block.soundGrassFootstep);
+		skyDirt = new BlockSkyDirt(ConfigManager.idDirt, 2).setHardness(0.5F).setResistance(2.0f).setStepSound(Block.soundGravelFootstep);
+		skyFlower = new BlockSkyFlower(ConfigManager.idFlower, 5).setHardness(0.0F).setStepSound(Block.soundGrassFootstep);
+		present = new BlockPresent(ConfigManager.idPresent).setHardness(0.4F);
+		skyWood = new BlockSkyWood(ConfigManager.idWood).setHardness(0.7F);
 
 		
 		registerBlocks();
 		
 
 }
-	@SuppressWarnings("deprecation")
+	
 	public void registerBlocks(){
-		GameRegistry.registerBlock(portal);
-		GameRegistry.registerBlock(portalTrigger);
-		GameRegistry.registerBlock(skyStone);
-		GameRegistry.registerBlock(skyGrass);
-		GameRegistry.registerBlock(skyDirt);
-		GameRegistry.registerBlock(skyFlower);
-		GameRegistry.registerBlock(present);
-		GameRegistry.registerBlock(skyWood);
+		GameRegistry.registerBlock(portal,"portal");
+		GameRegistry.registerBlock(portalTrigger,"portalTrigger");
+		GameRegistry.registerBlock(skyStone,"skyStone");
+		GameRegistry.registerBlock(skyGrass,"skyGrass");
+		GameRegistry.registerBlock(skyDirt,"skyDirt");
+		GameRegistry.registerBlock(skyFlower,"skyFlower");
+		GameRegistry.registerBlock(present,"present");
+		GameRegistry.registerBlock(skyWood,"skyWood");
 
 	}
 }
